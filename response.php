47132ba6306fa112c3654755d9413115a8608037
shciafffs <111536843+shciafffs@users.noreply.github.com>
2022-09-22 15:40:48 -0400

    Update response.php

--- response.php
+++ response.php
@@ -3,11 +3,11 @@
     <head>
     </head>
     <body>
-        <p>Your First Name is: <?=($_POST['FNAME']) ?></p><br>
-        <p>Your Last Name is: <?=($_POST['LNAME']) ?></p><br>
-        <p>Your UserName is: <?=($_POST['USERNAME']) ?></p><br>
-        <p>Your Password is: <?=($_POST['PASSWORD']) ?></p><br>
-        <p>Your Gender is: <?=($_POST['SEX']) ?></p><br>
-        <p>Your Conditions are: <?=($_POST['CONDITIONS']) ?></p><br>
+        <p>Your First Name is: <?= htmlspecialchars($_POST['FNAME']) ?></p><br>
+        <p>Your Last Name is: <?= htmlspecialchars($_POST['LNAME']) ?></p><br>
+        <p>Your UserName is: <?= htmlspecialchars($_POST['USERNAME']) ?></p><br>
+        <p>Your Password is: <?= htmlspecialchars($_POST['PASSWORD']) ?></p><br>
+        <p>Your Gender is: <?= htmlspecialchars($_POST['SEX']) ?></p><br>
+        <p>Your Conditions are: <?= htmlspecialchars($_POST['CONDITIONS']) ?></p><br>
     </body>
 </html>
