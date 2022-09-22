c079095c7f17216746798bab8ce975f739f4ca72
shciafffs <111536843+shciafffs@users.noreply.github.com>
2022-09-22 15:14:56 -0400

    more sophisticated response.php

--- response.php
+++ response.php
@@ -3,7 +3,11 @@
     <head>
     </head>
     <body>
-        <p><?=var_dump($_GET) ?></p>
-        <p><?=var_dump($_POST) ?></p>
+        <p>Your First Name is: <?=var_dump($_POST['FNAME']) ?></p><br>
+        <p>Your Last Name is: <?=var_dump($_POST['LNAME']) ?></p><br>
+        <p>Your UserName is: <?=var_dump($_POST['USERNAME']) ?></p><br>
+        <p>Your Password is: <?=var_dump($_POST['PASSWORD']) ?></p><br>
+        <p>Your Gender is: <?=var_dump($_POST['SEX']) ?></p><br>
+        <p>Your Conditions are: <?=var_dump($_POST['CONDITIONS']) ?></p><br>
     </body>
 </html>
