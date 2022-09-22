5505d9d4d253d30791e7e51fdf146677f772e9e0
shciafffs <111536843+shciafffs@users.noreply.github.com>
2022-09-22 14:34:02 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
-<DOCTYPE !HTML>
+<!DOCTYPE HTML>
 <html>
     <head>
         <title>TEST PHP</title>
     </head>
     <body>
-        <p><?=var_dump($_SERVER['HTTP_USER_AGENT']) ?><p>
+        <p>Your User-Agent is: <?=var_dump($_SERVER['HTTP_USER_AGENT']) ?><p>
     </body>
 </html>
