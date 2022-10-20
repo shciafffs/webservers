7ed63a4b20785c44711b8d81e0d1431e272c6999
shciafffs <111536843+shciafffs@users.noreply.github.com>
2022-10-20 15:34:52 -0400

    Update response.php

--- response.php
+++ response.php
@@ -1,6 +1,24 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+    <?php
+            $server="localhost";
+            $username="alessio";
+            $password="Password01";
+            $database="userinfo";
+            $conn=mysqli_connect($server, $username, $password, $database);
+
+            $fname=htmlspecialchars($_POST['FNAME']);
+            $lname=htmlspecialchars($_POST['LNAME']);
+            $username=htmlspecialchars($_POST['USERNAME']);
+            $password=htmlspecialchars($_POST['PASSWORD']);
+            $sex=htmlspecialchars($_POST['SEX']);
+            $conditions=htmlspecialchars($_POST['CONDITIONS']);
+
+
+            $sql="INSERT INTO users (FNAME, LNAME, SEX, USERNAME, PASSWORD, CONDITIONS) values('$fname', '$lname', $sex, '$username', '$password', '$conditions');";
+            $result=mysqli_query($conn, $sql);
+    ?>
     </head>
     <body>
         <p>Your First Name is: <?= htmlspecialchars($_POST['FNAME']) ?></p><br>
